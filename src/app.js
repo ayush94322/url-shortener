93186bef29e62b8944df84c8f511bd477a358565
akydev890-commits <akydev890@gmai.com>
2026-07-12 01:43:22 +0530

    feat: register api/urls route in app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
+import urlRoutes from "./routes/url.routes.js";
+
 const app = express();
 
 // Middlewares
@@ -11,11 +13,22 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use("/api/urls", urlRoutes);
+
 // Health check route
 app.get("/health", (req, res) => {
   res.status(200).json({
     success: true,
     message: "Server is running",
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error"
   });
 });
 
